ad0add07284eb47e91287fbd102f64f16ed45d50
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-28 18:16:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function checkCollision(rock) {
     return (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
       (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
       (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
-    )
+    );
     }
   }
 
